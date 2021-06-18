d57842d6bc069f6e6d00f7e539f162bceab22bea
Alejandro-DP <alexdelacruz1888@gamil.com>
2021-06-18 01:31:47 -0500

    httpclient

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,17 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  
+
+   listado;
+  constructor(private http: HttpClient) {
+
+    this.http.get("http://localhost/IonicDb/consultados.php").subscribe(snap =>{
+
+      console.log(snap);
+      this.listado=snap;
+    });
+
+  }
 
 }
