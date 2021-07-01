9cfabe65867a78c001480a42133d20a13453ed69
Demon-fe <franciscoe676@gmail.com>
2021-07-01 17:59:27 -0500

    actualizacion

--- src/app/crear-cuenta/crear-cuenta.page.ts
+++ src/app/crear-cuenta/crear-cuenta.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController, NavParams } from '@ionic/angular';
 
 @Component({
   selector: 'app-crear-cuenta',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crear-cuenta.page.scss'],
 })
 export class CrearCuentaPage implements OnInit {
-
+usuario: string ="";
+email: string ="";
+sexo: string ="";
+password: string ="";
   constructor() { }
-
+  
   ngOnInit() {
   }
-
+ionViewDidEnter(){
+}
 }
