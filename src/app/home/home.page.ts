3edc501cea49e265f2d73b3ea31cf2565807e119
Demon-fe <franciscoe676@gmail.com>
2021-07-02 02:24:48 -0500

    Revert "comim"
    This reverts commit 36e763cf910cb7acf117951599fc55ff21c3a1e7, reversing
    changes made to 9cfabe65867a78c001480a42133d20a13453ed69.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import {HttpClient } from '@angular/common/http';
-
 
 @Component({
   selector: 'app-home',
@@ -9,28 +7,6 @@ import {HttpClient } from '@angular/common/http';
 })
 export class HomePage {
 
-  
+  constructor() {}
 
-   listado;
-  constructor(private http: HttpClient) {
-
-   
-
-  }
-
-
-  public login(event: any){
-/*     this.http.post<any>('https://reqres.in/api/posts', { title: 'Angular POST Request Example' }).subscribe(data => {
-        this.postId = data.id;
-    }) */
-      this.http.post<any>("http://localhost/IonicDb/validar.php", {
-        
-        user:event.target.user.value,
-        pass:event.target.pass.value
-      }).subscribe(data => {
-        console.log(data)
-    }) ;
-    
- 
-  }
 }
